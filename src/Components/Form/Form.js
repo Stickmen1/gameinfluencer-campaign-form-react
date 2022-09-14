97630529d26bc85190976fbfce0746983ae7876a
ouplex <momchil26@gmail.com>
2022-09-14 12:55:14 +0300

    Initial commit

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,41 +1,64 @@
 import React, {Component, useState} from 'react';
 import "./Form.css";
 
+const OtherGame = (props) => (
+    <div id="otherGameDiv" >
+        <label htmlFor="otherGame">Other game:</label>
+        <input type="text" id="otherGame" name="otherGame" value={props.campaignGame} onChange={(event) => {props.changeValue(event.target.value)}}/>
+    </div>
+)
+
 class Form extends Component {
     constructor(props) {
         super(props);
-    }
-
-    state = {
-        campaignName: '',
-        campaignDescription: '',
-        campaignGame: '',
-        campaignBudget: 0,
-        campaignStartDate: new Date(),
-        campaignEndDate: new Date(),
-        campaignLanguage: '',
-        formInformation: [],
+        this.state = {
+                campaignName: '',
+                campaignDescription: '',
+                campaignBudget: 0,
+                campaignGame: '',
+                otherText: '',
+                campaignStartDate: new Date(),
+                campaignEndDate: new Date(),
+                campaignLanguage: '',
+                campaigns: [],
+        }
     }
 
     handleSubmit(event) {
-       //let campaignOldInformation =
+        console.log(this.state);
+        this.setState({campaigns: [...this.state.campaigns, {
+                campaignName: this.state.campaignName,
+                campaignGame: this.state.campaignGame === "Other" ? this.state.otherText : this.state.campaignGame,
+                campaignBudget: this.state.campaignBudget,
+                campaignStartDate: this.state.campaignStartDate,
+                campaignLanguage: this.state.campaignLanguage}],
+                campaignName: '',
+                campaignDescription: '',
+                campaignGame: '',
+                campaignBudget: '',
+                campaignStartDate: new Date(),
+                campaignEndDate: new Date(),
+                campaignLanguage: '',
+                otherText: '',
+        }
+        )
     }
 
     render() {
         return (
             <div className={"form_section"}>
-                <form className="campaignForm" onSubmit={this.handleSubmit}>
+                <form className="campaignForm">
                     <label htmlFor="campaignName">Campaign name:</label><br />
-                    <input type="text" value={this.state.campaignName} onChange={(event) => {this.setState({...this.state, campaignName: event.target.value})}} placeholder="Your campaign name..."/>
+                    <input type="text" value={this.state.campaignName} onChange={(event) => {this.setState({campaignName: event.target.value})}} placeholder="Your campaign name..."/>
                     <p id="errorHelperName">Your campaign name should be between 1 and 20 characters!</p>
                     <label htmlFor="campaignDescription">Campaign description:</label><br />
                     <hr />
 
-                    <textarea id="campaignDescription" name="campaignDescription" value={this.state.campaignDescription} onChange={(event) => {this.setState({...this.state, campaignDescription: event.target.value})}} rows="6" cols="20" placeholder="Your campaign description..." required></textarea>
+                    <textarea id="campaignDescription" name="campaignDescription" value={this.state.campaignDescription} onChange={(event) => {this.setState({campaignDescription: event.target.value})}} rows="6" cols="20" placeholder="Your campaign description..." required></textarea>
                     <hr />
 
                         <label htmlFor="campaignGame">Campaign game:</label><br />
-                        <select id="campaignGame" name="campaignGame" value={this.state.campaignGame} onChange={(event) => {this.setState({...this.state, campaignGame: event.target.value})}} required>
+                        <select id="campaignGame" name="campaignGame" value={this.state.campaignGame} onChange={(event) => {this.setState({campaignGame: event.target.value})}} required>
                             <option value="" disabled selected>Select the game</option>
                             <option value="League of Legends">League of Legends</option>
                             <option value="DOTA 2">DOTA 2</option>
@@ -44,15 +67,15 @@ class Form extends Component {
                             <option value="Apex Legends">Apex Legends</option>
                             <option id="otherGameSelected" value="Other">Other</option>
                         </select>
-
+                    {this.state.campaignGame === "Other" && (<OtherGame campaignGame={this.state.otherText} changeValue={(value) => {this.setState({otherText: value})}}/>)}
                     <label htmlFor="campaignBudget">Campaign budget:</label>
-                    <input type="number" id="campaignBudget" name="campaignBudget" value={this.state.campaignBudget} onChange={(event) => {this.setState({...this.state, campaignBudget: event.target.value})}} min="1" max="20000" placeholder="Your campaign budget..." required />
+                    <input type="number" id="campaignBudget" name="campaignBudget" value={this.state.campaignBudget} onChange={(event) => {this.setState({campaignBudget: event.target.value})}} min="1" max="20000" placeholder="Your campaign budget..." required />
 
                     <label htmlFor="campaignStartDate">Campaign start date:</label>
-                    <input type="date" id="campaignStartDate" name="campaignStartDate" value={this.state.campaignStartDate} onChange={(event) => {this.setState({...this.state, campaignStartDate: event.target.value})}} required /><br />
+                    <input type="date" id="campaignStartDate" name="campaignStartDate" value={this.state.campaignStartDate} onChange={(event) => {this.setState({campaignStartDate: event.target.value})}} required /><br />
 
                     <label htmlFor="campaignEndDate">Campaign end date:&nbsp</label>
-                    <input type="date" id="campaignEndDate" name="campaignEndDate" value={this.state.campaignEndDate} onChange={(event) => {this.setState({...this.state, campaignEndDate: event.target.value})}} required />
+                    <input type="date" id="campaignEndDate" name="campaignEndDate" value={this.state.campaignEndDate} onChange={(event) => {this.setState({campaignEndDate: event.target.value})}} required />
                     <hr />
 
                     <p id="questionCrypto">Can the campaign be paid with crypto currency?</p>
@@ -63,7 +86,7 @@ class Form extends Component {
                     <hr />
 
                     <label htmlFor="campaignLanguage">Campaign language:</label><br />
-                    <select id="campaignLanguage" name="campaignLanguage" value={this.state.campaignLanguage} onChange={(event) => {this.setState({...this.state, campaignLanguage: event.target.value})}} required>
+                    <select id="campaignLanguage" name="campaignLanguage" value={this.state.campaignLanguage} onChange={(event) => {this.setState({campaignLanguage: event.target.value})}} required>
                         <option value="" disabled selected>Select the language</option>
                         <option value="English">English</option>
                         <option value="German">German</option>
@@ -72,18 +95,30 @@ class Form extends Component {
                     </select>
                     <hr />
                 </form>
-                <input type="submit" value={"Submit"}/>
+                <input type="submit" value={"Submit"} onClick={this.handleSubmit.bind(this)}/>
                 <hr />
                 <table id="campaignTable">
                     <thead>
-                        <th>Campaign name</th>
-                        <th>Campaign game</th>
-                        <th>Campaign start date</th>
-                        <th>Campaign budget</th>
-                        <th>Campaign language</th>
+                        <tr>
+                            <th>Campaign name</th>
+                            <th>Campaign game</th>
+                            <th>Campaign start date</th>
+                            <th>Campaign budget</th>
+                            <th>Campaign language</th>
+                        </tr>
                     </thead>
                     <tbody>
-                        {this.state.formInformation.map(campaignInformation => <td>{campaignInformation}</td>)}
+                    {this.state.campaigns.map((value) => {
+                        return (
+                            <tr>
+                                <td>{value.campaignName}</td>
+                                <td>{value.campaignGame}</td>
+                                <td>{value.campaignStartDate}</td>
+                                <td>{value.campaignBudget}</td>
+                                <td>{value.campaignLanguage}</td>
+                            </tr>
+                        )
+                    })}
                     </tbody>
                 </table>
             </div>
@@ -93,9 +128,3 @@ class Form extends Component {
 
 export default Form;
 
-const OtherGame = () => (
-    <div id="otherGameDiv" >
-        <label htmlFor="otherGame">Other game:</label>
-        <input type="text" id="otherGame" name="otherGame"/>
-    </div>
-)
